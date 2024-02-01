da88e20ea75292e5c172924915a7c95bcddd85eb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 15:10:45 +0900

    chore: Change React.StrictMode to BrowserRouter for wrapp in index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+
+  // BrowserRouter로 루트 컴포넌트를 감싸준다.
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
