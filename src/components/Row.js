e87b8cdd9c1e289aceede67e5a4860126b5731d5
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 16:01:41 +0900

    chore: Remove fetchMovieData function empty array in Row.js file

--- src/components/Row.js
+++ src/components/Row.js
@@ -11,10 +11,11 @@ const Row = ({ id, title, fetchUrl, isLargeRow }) => {
 
     useEffect(() => {
         fetchMovieData();
-    }, [fetchUrl]);
+    }, []);
 
     const fetchMovieData = async () => {
         const request = await axios.get(fetchUrl);
+        console.log("request", request);
         setMovies(request.data.results);
     }
 
@@ -51,50 +52,3 @@ const Row = ({ id, title, fetchUrl, isLargeRow }) => {
 }
 
 export default Row;
-
-{/* <section className="row">
-            <h2>{title}</h2>
-            <Swiper 
-            // install Swiper modules
-            modules={[Navigation, Pagination, Scrollbar, A11y]}
-            loop={true} // loop 기능을 사용할 것인지
-            breakpoints={{
-                1378: {
-                slidesPerView: 6, // 한번에 보이는 슬라이드 개수
-                slidesPerGroup: 6, // 몇개씩 슬라이드 할지
-                },
-                998: {
-                slidesPerView: 5,
-                slidesPerGroup: 5,
-                },
-                625: {
-                slidesPerView: 4,
-                slidesPerGroup: 4,
-                },
-                0: {
-                slidesPerView: 3,
-                slidesPerGroup: 3,
-                },
-            }}
-            navigation  // arrow 버튼 사용 유무 
-            pagination={{ clickable: true }} // 페이지 버튼 보이게 할지 
-            >
-            <div id={id} className="row__posters">
-            {movies.map((movie) => (
-                <SwiperSlide>
-                <img
-                    key={movie.id}
-                    style={{ padding: "25px 0" }}
-                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                    src={`https://image.tmdb.org/t/p/original/${
-                    isLargeRow ? movie.poster_path : movie.backdrop_path
-                    } `}
-                    alt={movie.name}
-                    onClick={() => handleClick(movie)}
-                />
-                </SwiperSlide>
-            ))}
-            </div>
-        </Swiper>
-        {modalOpen && ( <MovieModal {...movieSelected} setModalOpen={setModalOpen} /> )}
-        </section> */}
