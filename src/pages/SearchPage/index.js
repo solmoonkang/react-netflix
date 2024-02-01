593b8e9ee92c35fac304faac683761a81d69a651
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 15:45:30 +0900

    feat: Implement get search term from search page in index.js file

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "../../api/axios";
 
 export default function SearchPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [searchResults, setSearchResults] = useState([]);
+
+    let query = useQuery();
+    const searchTerm = query.get("q");
+
+    useEffect(() => {
+        if (searchTerm) {
+            fetchSearchMovie(searchTerm);
+        }
+    }, [searchTerm]);
+
+    const fetchSearchMovie = async (searchTerm) => {
+        try {
+            const request = await axios.get(`/search/multi?include_adult=false&query=$(searchTerm)`);
+            console.log(request);
+            setSearchResults(request.data.results);
+        } catch (error) {
+            console.log("ERROR", error);
+        }
+    }
+
+    const useQuery = () => {
+        return new URLSearchParams(useLocation().search);
+    }
+
+    return (
+        <div>
+          
+        </div>
+    );
 }
