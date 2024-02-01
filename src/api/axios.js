f342eb33e1b1ae12b799bca5fb25c618263cbbd6
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 17:47:59 +0900

    feat: Implement hide api key in axios.js file

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
     params: {
-        api_key: "83afe8eb888a67d46eaf30c7b7d67e9b",
+        api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
         language: "ko-KR",
     },
 });
