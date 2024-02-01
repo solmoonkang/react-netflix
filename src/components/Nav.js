933b98321448f56343f166b6db3f6eb6ff96420b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 15:34:13 +0900

    feat: Implement nav component for search input function in Nav.js file

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 export default function Nav() {
 
     const [show, setShow] = useState(false);
+    const [searchValue, setSearchValue] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         window.addEventListener("scroll", () => {
@@ -21,6 +24,11 @@ export default function Nav() {
         };
     }, []);
 
+    const handleChange = (e) => {
+        setSearchValue(e.target.value);
+        navigate(`/search?q=${e.target.value}`);
+    }
+
 
     return (
         <nav className={`nav ${show && "nav__black"}`}>
@@ -28,13 +36,14 @@ export default function Nav() {
             alt="Netflix logo" 
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png" 
             className="nav__logo" 
-            onClick={() => window.location.reload()} 
-            />
+            onClick={() => window.location.reload()} />
+
+            <input className="nav__input" value={searchValue} onChange={handleChange} type="text" placeholder="영화를 검색해주세요." />
+
             <img 
             alt="User logged" 
             src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41" 
-            className="nav__avatar" 
-            />
+            className="nav__avatar" />
         </nav>
       );
 }
