588e908c37eb120418aff2b811af4e7780f1ae6a
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 12:31:25 +0900

    feat: Add import the Row file in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import './App.css';
-import Banner from './components/Banner';
-import Nav from './components/Nav';
+import "./App.css";
+import axios from "./api/axios";
+import requests from "./api/requests";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Banner />
+      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} largeRow />
+      <Row title="Trending Now" if="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" if="TR" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" if="AM" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" if="CM" fetchUrl={requests.fetchComedyMovies} />
     </div>
   );
 }
