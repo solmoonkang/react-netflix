42564e78ca1ebfaf912d0632a48cc2bc781cd1bb
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 18:11:26 +0900

    feat: Implement swiper in Row.js file

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,8 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "../api/axios";
 import MovieModal from "./MovieModal";
 import "./Row.css";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-const Row = ({ id, title, fetchUrl, isLargeRow }) => {
+export default function Row({ id, title, fetchUrl, isLargeRow }) {
 
     const [movies, setMovies] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
@@ -27,28 +32,34 @@ const Row = ({ id, title, fetchUrl, isLargeRow }) => {
     return (
         <section className="row">
             <h2>{title}</h2>
-            <div className="slider">
-                <div className="slider__arrow-left">
-                    <span className="arrow" onClick={() => {document.getElementById(id).scrollLeft -= window.innerWidth - 80;}}>{"<"}</span>
-                </div>
+            <Swiper 
+                modules={[Navigation, Pagination, Scrollbar, A11y]}     // install Swiper modules
+                loop={true}                                             // loop 기능을 사용할 것인지
+                navigation                                              // arrow 버튼 사용 유무 
+                pagination={{ clickable: true }}                        // 페이지 버튼 보이게 할지 
+                breakpoints={{
+                1378: {slidesPerView: 6, slidesPerGroup: 6},
+                998: {slidesPerView: 5, slidesPerGroup: 5},
+                625: {slidesPerView: 4, slidesPerGroup: 4},
+                0: {slidesPerView: 3, slidesPerGroup: 3},
+                }}
+            >
                 <div id={id} className="row__posters">
-                {movies.map((movie) => (
-                <img
-                    key={movie.id}
-                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-                    src={`https://image.tmdb.org/t/p/original/${ isLargeRow ? movie.poster_path : movie.backdrop_path } `}
-                    alt={movie.name}
-                    onClick={() => handleClick(movie)}
-                />
-                ))}
+                    {movies.map((movie) => (
+                        <SwiperSlide>
+                            <img
+                                key={movie.id}
+                                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                                style={{ padding: "25px 0" }}
+                                onClick={() => handleClick(movie)}
+                                src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+                                alt={movie.name}
+                            />
+                        </SwiperSlide>
+                    ))}
                 </div>
-                <div className="slider__arrow-right">
-                    <span className="arrow" onClick={() => {document.getElementById(id).scrollLeft += window.innerWidth - 80;}}>{">"}</span>
-                </div>
-            </div>
+            </Swiper>
             {modalOpen && (<MovieModal {...movieSelected} setModalOpen={setModalOpen} />)}
         </section>
     );
 }
-
-export default Row;
