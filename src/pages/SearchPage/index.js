8ff202fb41ce29023356af2ef236734352f4f56b
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-02-01 16:15:19 +0900

    feat: Implement renderSearchResults function if saerch term has movie data or not in index.js file

--- src/pages/SearchPage/index.js
+++ src/pages/SearchPage/index.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
 
 export default function SearchPage() {
 
     const [searchResults, setSearchResults] = useState([]);
+    const navigate = useNavigate();
     const useQuery = () => {
         return new URLSearchParams(useLocation().search);
     }
@@ -28,9 +29,30 @@ export default function SearchPage() {
         }
     }
 
-    return (
-        <div>
-          
-        </div>
-    );
+    const renderSearchResults = () => {
+        return searchResults.length > 0 ? (
+            <section className="search-container">
+                {searchResults.map((movie) => {
+                    if (movie.backdrop.path !== null && movie.media_type !== "person") {
+                        const movieImageUrl = "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+                        return (
+                            <div className="movie" key={movie.id}>
+                                <div className="movie__column-poster" onClick={() => navigate(`/${movie.id}`)}>
+                                    <img src={movieImageUrl} alt="movie" className="movie__poster" />
+                                </div>
+                            </div>
+                        );
+                    }
+                })}
+            </section>
+        ) : (
+            <section className="no-results">
+                <div className="no-results__text">
+                    <p>찾고자 하는 검색어 "{searchTerm}"에 맞는 영화가 없습니다.</p>
+                </div>
+            </section>
+        );
+    }
+
+    return renderSearchResults();
 }
